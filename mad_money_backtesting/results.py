01f85515af6bc3fde4a9d9a68b10b8c612e6ab0f
Gabor Vecsei <gabor.vecsei@doclerholding.com>
2021-05-28 14:51:54 +0200

    pagination added to the html table

--- mad_money_backtesting/results.py
+++ mad_money_backtesting/results.py
@@ -17,17 +17,21 @@ def summarize_backtesting_results(results: List[pd.Series],
     for i, strat_name in enumerate(results_df["_strategy"].values):
         strat_name = str(strat_name)
         name, parameters = strat_name.split("(")
-        if symbols is not None:
-            name = f"{name} ({symbols[i]})"
         parameters = "(" + parameters
         simplified_strategy_names.append(name)
         strategy_parameters.append(parameters)
 
+    if symbols is not None:
+        if len(symbols) != len(results_df):
+            raise ValueError("The number of symbols is not matching the number of rows in the df")
+        results_df["Symbol"] = symbols
+
     results_df["Strategy"] = simplified_strategy_names
     if include_parameters:
         results_df["Parameters"] = strategy_parameters
+
     results_df.drop(columns="_strategy", inplace=True)
-    results_df.set_index("Strategy", inplace=True)
+    results_df.set_index("Symbol", inplace=True)
 
     if sort_by is not None:
         results_df.sort_values(sort_by, inplace=True, ascending=False)
